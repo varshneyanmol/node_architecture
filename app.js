e305f6a68ae39f0e6246f40722a706eb96ef40b2
varshneyanmol <anmol.varshney@askiitians.com>
2019-10-07 12:29:47 +0530

    minor

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-const cluster = require('cluster');
+// const cluster = require('cluster');
+//
+// if (cluster.isMaster) {
+//     require('./master_init');
+// } else {
+//     require('./express_init');
+// }
 
-if (cluster.isMaster) {
-    require('./master_init');
-} else {
-    require('./express_init');
-}
+// var cluster = require('cluster')
+//     , app = require('./demo');
+//
+// cluster('./demo')
+//     .listen(3000);
+
+// cluster('./demo')
+//     .use(cluster.logger('logs'))
+//     .use(cluster.stats())
+//     .use(cluster.pidfiles('pids'))
+//     .use(cluster.cli())
+//     .use(cluster.repl(8888))
+//     .listen(3000);
+
+
+var cluster = require('cluster-mode');
+var config = {
+    max: 2 // start the application with 8 workers
+};
+cluster.start(config, function () {
+    // cluster process is ready
+});
+
+
--- services/RedisExpireEvent.js
+++ services/RedisExpireEvent.js
@@ -1,10 +1,29 @@
-class RedisExpireEvent extends require('./RedisEvent'){
+const redisEventConstants = require('./redis_event_constants');
+
+class RedisExpireEvent extends require('./RedisEvent') {
     constructor(redisClientListener, redisClientScheduler, db = 0) {
         super(redisClientListener, db);
         this.scheduler = redisClientScheduler;
+        super.subscribeToEvents(redisEventConstants.EXPIRE)
     }
 
+    _getMillis(expiration) {
+        if (expiration instanceof Date) {
+            var now = new Date().getTime();
+            expiration = expiration.getTime() - now;
+        }
+        return expiration;
+    };
 
+    async schedule(key, ttl, handler) {
+        try {
+            const result = await this.scheduler.set(key, '', 'PX', this._getMillis(ttl));
+            super.register(redisEventConstants.EXPIRE, key, handler);
+
+        } catch (err) {
+            throw err;
+        }
+    }
 }
 
 module.exports = RedisExpireEvent;
